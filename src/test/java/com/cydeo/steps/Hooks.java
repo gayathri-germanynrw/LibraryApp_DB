316a015adc52236780619ee2dce8c3e5a7889753
gayat <gayathrikasi@gmail.com>
2024-06-01 20:09:07 +0200

    Library App for DB added

---
+++ src/test/java/com/cydeo/steps/Hooks.java
@@ -0,0 +1,51 @@
+package com.cydeo.steps;
+
+import com.cydeo.utility.ConfigurationReader;
+import com.cydeo.utility.DB_Util;
+import com.cydeo.utility.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+
+        System.out.println("this is coming from BEFORE");
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
+
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        System.out.println("this is coming from AFTER");
+
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+
+    }
+
+    @Before("@db")
+    public void setupDB(){
+        System.out.println("Connecting to database...");
+        DB_Util.createConnection();
+    }
+
+    @After("@db")
+    public void closeDB(){
+        System.out.println("Closing DB connection...");
+        DB_Util.destroy();
+    }
+}
